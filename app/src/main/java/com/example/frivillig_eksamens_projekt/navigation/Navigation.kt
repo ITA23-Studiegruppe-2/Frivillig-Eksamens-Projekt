8c61d0f92679ad3c39d0597bbec6978d280ee0ed
Planteig <Planteig17@hotmail.com>
2024-04-26 14:59:44 +0200

    Added Navigation files

---
+++ app/src/main/java/com/example/frivillig_eksamens_projekt/navigation/Navigation.kt
@@ -0,0 +1,38 @@
+package com.example.frivillig_eksamens_projekt.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "") {
+
+        //Home Screen
+        composable(Screen.Home.route) {
+            // ADD HOMESCREEN COMPOSEABLE
+        }
+
+        // Start Screen
+        composable(Screen.Start.route) {
+            // ADD START SCREEN COMPOSABLE
+        }
+
+        // Login Screen
+        composable(Screen.Login.route) {
+            // ADD LOGIN SCREEN COMPOSEABLE
+        }
+        // Register Screen
+        composable(Screen.Register.route) {
+            // ADD REGISTER SCREEN COMPOSEABLE
+        }
+
+    }
+}
