9989ed2e45d6091fd2ffc8275d50c3d4977a585e
LucasFJ-2023 <lfjjac@gmail.com>
2024-04-26 16:10:14 +0200

    asd

--- app/src/main/java/com/example/frivillig_eksamens_projekt/MainActivity.kt
+++ app/src/main/java/com/example/frivillig_eksamens_projekt/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.frivillig_eksamens_projekt.navigation.Navigation
 import com.example.frivillig_eksamens_projekt.ui.loginScreen.LoginScreen
 import com.example.frivillig_eksamens_projekt.ui.navigationBar.BottomNavigationBar
+import com.example.frivillig_eksamens_projekt.ui.registerScreen.CreateUserScreen
 import com.example.frivillig_eksamens_projekt.ui.registerScreen.UserOrOrganisation
 import com.example.frivillig_eksamens_projekt.ui_elements.theme.FrivilligEksamensProjektTheme
 
@@ -27,9 +28,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginScreen(
-                        onSuccessLogin = {}
-                    )
+
+                    CreateUserScreen()
+
                     BottomNavigationBar(
                         onSearchClick = { /*TODO*/ },
                         onCalenderClick = { /*TODO*/ },
@@ -38,9 +39,6 @@ class MainActivity : ComponentActivity() {
                         
                     }
 
-
-
-                    Navigation()
                 }
             }
         }
