c2c06c2c806ca3c1e73e49ddd52cdf00cd13d7f0
Planteig <Planteig17@hotmail.com>
2024-04-26 15:10:54 +0200

    Added accountService and navigation bar

--- app/src/main/java/com/example/frivillig_eksamens_projekt/MainActivity.kt
+++ app/src/main/java/com/example/frivillig_eksamens_projekt/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.frivillig_eksamens_projekt.ui.theme.FrivilligEksamensProjektTheme
+import com.example.frivillig_eksamens_projekt.navigation.Navigation
+import com.example.frivillig_eksamens_projekt.ui.navigationBar.BottomNavigationBar
+import com.example.frivillig_eksamens_projekt.ui_elements.theme.FrivilligEksamensProjektTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +25,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    BottomNavigationBar(
+                        onSearchClick = { /*TODO*/ },
+                        onCalenderClick = { /*TODO*/ },
+                        onHomePageClick = { /*TODO*/ },
+                        onEmailClick = { /*TODO*/ }) {
+                        
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FrivilligEksamensProjektTheme {
-        Greeting("Android")
     }
 }
